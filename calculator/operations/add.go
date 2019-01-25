80a80912606b8534dbf331ee339d20745daa42a4
nicobidone <nicobidone@gmail.com>
2019-01-25 18:04:20 -0300

    Deleted operations

--- calculator/operations/add.go
+++ calculator/operations/add.go
@@ -4,18 +4,3 @@ package operations
 func Add(a int, b int) int {
 	return int(a + b)
 }
-
-// Div returns its argument int as additions from a & b.
-func Div(a int, b int) int {
-	return int(a / b)
-}
-
-// Sub returns its argument int as additions from a & b.
-func Sub(a int, b int) int {
-	return int(a - b)
-}
-
-// Mul returns its argument int as additions from a & b.
-func Mul(a int, b int) int {
-	return int(a * b)
-}
