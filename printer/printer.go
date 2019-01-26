4c84c2a2c23c6899ccefca4e2df0e93bb626c618
nicobidone <nicobidone@gmail.com>
2019-01-25 21:42:01 -0300

    Edited to receive any argument

--- printer/printer.go
+++ printer/printer.go
@@ -2,42 +2,39 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
 )
+
+func whatType(v interface{}) {
+	fmt.Printf("type of %v: %T\n", v, v)
+}
+
+type str struct{}
 
 func main() {
 
-	_, err := strconv.Atoi(os.Args[1])
-	if err == nil {
-		fmt.Println("integer")
-		return
-	}
-	b := os.Args[1]
-	if b == "true" || b == "false" {
-		fmt.Println("bool")
-		return
-	}
-	_, err2 := strconv.ParseFloat(os.Args[1], 64)
-	if err2 == nil {
-		fmt.Println("float64")
-		return
-	}
-	fmt.Println("string")
-
 	/*
-		wordPtr := flag.String("word", "foo", "a string")
-		numbPtr := flag.Int("numb", 42, "an int")
-		boolPtr := flag.Bool("fork", false, "a bool")
-		floatPtr := flag.Float64("float64", 0.1, "a float")
-		var svar string
-		flag.StringVar(&svar, "svar", "bar", "a string var")
-		flag.Parse()
-		fmt.Println("word:", *wordPtr)
-		fmt.Println("numb:", *numbPtr)
-		fmt.Println("fork:", *boolPtr)
-		fmt.Println("lefloat", *floatPtr)
-		fmt.Println("svar:", svar)
-		fmt.Println("tail:", flag.Args())
+		_, err := strconv.Atoi(os.Args[1])
+		if err == nil {
+			fmt.Println("integer")
+			return
+		}
+		b := os.Args[1]
+		if b == "true" || b == "false" {
+			fmt.Println("bool")
+			return
+		}
+		_, err2 := strconv.ParseFloat(os.Args[1], 64)
+		if err2 == nil {
+			fmt.Println("float64")
+			return
+		}
+		fmt.Println("string")
 	*/
+	s := str{}
+	whatType(true)
+	whatType(s)
+	whatType(3)
+	whatType("hi")
+	whatType(5.3)
+
 }
