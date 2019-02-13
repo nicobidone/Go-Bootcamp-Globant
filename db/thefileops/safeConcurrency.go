a1929112894e56ab025371ee6760567859bf4fe7
nicobidone <nicobidone@gmail.com>
2019-02-13 17:26:42 -0300

    Comments added

--- db/thefileops/safeConcurrency.go
+++ db/thefileops/safeConcurrency.go
@@ -15,24 +15,28 @@ func NewSafeFile(path string) *SafeFile {
 	return s
 }
 
+// SafeCreateFile applies mutex on a specific file
 func (s *SafeFile) SafeCreateFile() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	CreateFile(s.path)
 }
 
+// SafeDeleteFile applies mutex on a specific file
 func (s *SafeFile) SafeDeleteFile() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	DeleteFile(s.path)
 }
 
+// SafeReadFile applies mutex on a specific file
 func (s *SafeFile) SafeReadFile() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	ReadFile(s.path)
 }
 
+// SafeWriteFile applies mutex on a specific file
 func (s *SafeFile) SafeWriteFile(text string) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
